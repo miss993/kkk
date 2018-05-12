7e1a5128d40223478a2188360095ef12bffe3bb2
wangyudong <482274154@qq.com>
2018-05-12 11:14:21 +0800

    update

---
+++ score-dao/src/test/java/edu/zut/cs/score/wangyudong/dao/StudentDaoTest.java
@@ -0,0 +1,14 @@
+package edu.zut.cs.score.wangyudong.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StudentDaoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
