0c0062028d4ef1b5badd27f6f71eb349957cf00e
csj <957022461@qq.com>
2018-05-20 20:41:24 +0800

    update

--- score-dao/src/test/java/edu/zut/cs/score/attendance/dao/StudentDaoTest.java
+++ score-dao/src/test/java/edu/zut/cs/score/attendance/dao/StudentDaoTest.java
@@ -1,5 +1,24 @@
 package edu.zut.cs.score.attendance.dao;
+import java.util.List;
 
-public class StudentDaoTest {
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.zut.cs.score.attendance.dao.StudentDao;
+import edu.zut.cs.score.attendance.domain.Student;
+import edu.zut.cs.score.base.dao.GenericDaoTestCase;
+
+public class StudentDaoTest extends GenericDaoTestCase<Long, Student, StudentDao>{
+	@Autowired
+	StudentDao studentDao;
+
+	@Test
+	public void testFindAll() {
+
+		List<Student> result = this.studentDao.findAll();
+
+	}
 
 }
+
+
