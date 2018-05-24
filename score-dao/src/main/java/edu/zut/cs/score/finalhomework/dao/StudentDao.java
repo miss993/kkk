15dfda8a48aa07d747bf607db08d4c2de2a4d7b7
chaixiaoyin <ruyanlive215@gmail.com>
2018-05-24 12:46:03 +0800

    修改

--- score-dao/src/main/java/edu/zut/cs/score/finalhomework/dao/StudentDao.java
+++
@@ -1,8 +0,0 @@
-package edu.zut.cs.score.finalhomework.dao;
-
-import edu.zut.cs.score.base.dao.GenericDao;
-import edu.zut.cs.score.finalhomework.domain.Student;
-
-public interface StudentDao extends GenericDao<Student, Long> {
-
-}
