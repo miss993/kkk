bef04e3ce7bf27a736e68705f90f1decd3f3cf6d
wangyudong <482274154@qq.com>
2018-06-02 19:37:15 +0800

    update web/spring/controller/FinalTestController.java

--- score-web/src/main/java/edu/zut/cs/score/finalscore/web/spring/controller/FinalTestController.java
+++ score-web/src/main/java/edu/zut/cs/score/finalscore/web/spring/controller/FinalTestController.java
@@ -1,5 +1,31 @@
 package edu.zut.cs.score.finalscore.web.spring.controller;
 
-public class FinalTestController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+import edu.zut.cs.score.base.web.spring.controller.GenericController;
+import edu.zut.cs.score.finalscore.domain.FinalTest;
+import edu.zut.cs.score.finalscore.service.FinalTestManager;
+
+@Controller
+@RequestMapping("/finalscore/finaltest")
+public class FinalTestController extends GenericController<FinalTest, Long, FinalTestManager>{
+
+	FinalTestManager finaltestManager;
+
+	@RequestMapping(method = RequestMethod.GET, value = "/index.html")
+	public String index() {
+		String result = "/finaltest/index";
+		return result;
+	}
+
+	@Autowired
+	public void setStudentManager(FinalTestManager finaltestManager) {
+		this.finaltestManager = finaltestManager;
+		this.manager = this.finaltestManager;
+	}
 
 }
