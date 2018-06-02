8ffe02517237e829e0eea8fa6e1d136dfc991c2f
wangyudong <482274154@qq.com>
2018-06-02 19:15:00 +0800

    finish the FinalTestManager.java

--- score-service/src/main/java/edu/zut/cs/score/finalscore/service/FinalTestManager.java
+++ score-service/src/main/java/edu/zut/cs/score/finalscore/service/FinalTestManager.java
@@ -1,5 +1,16 @@
 package edu.zut.cs.score.finalscore.service;
 
-public interface FinalTestManager {
+import java.util.List;
 
+import edu.zut.cs.score.base.service.GenericManager;
+import edu.zut.cs.score.finalscore.domain.FinalTest;
+
+public interface FinalTestManager extends GenericManager<FinalTest, Long>{
+
+	
+	
+	List<FinalTest> findBySno(String sno);
+	
+	
+	List<FinalTest> findBySname(String sname);
 }
