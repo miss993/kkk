28e0171b20ecf8ff79a92a5fc5cf7399d9f14b6c
wangyudong <482274154@qq.com>
2018-06-02 19:11:30 +0800

    dao/FinalTestDaoTest.java完成

--- score-dao/src/test/java/edu/zut/cs/score/fenzu/dao/FenzuTest.java
+++ score-dao/src/test/java/edu/zut/cs/score/fenzu/dao/FenzuTest.java
@@ -1,26 +1,6 @@
 package edu.zut.cs.score.fenzu.dao;
-import java.util.List;
 
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import edu.zut.cs.score.fenzu.domain.Fenzu;
-import edu.zut.cs.score.finalscore.dao.StudentDao;
-import edu.zut.cs.score.finalscore.domain.Student;
-import edu.zut.cs.score.base.dao.GenericDaoTestCase;
-import edu.zut.cs.score.fenzu.dao.FenzuDao;
-public class FenzuTest extends GenericDaoTestCase <Long,Fenzu,FenzuDao> {
-	@Autowired
-	StudentDao studentDao;
-
-	@Test
-	public void testFindAll() {
-
-		List<Student> result = this.studentDao.findAll();
-		if (logger.isInfoEnabled()) {
-            logger.info("testFindAll() - List<Student> result={}", result); //$NON-NLS-1$
-        }
-
-    }
+public class FenzuTest  {
 	
 
 }
--- score-dao/src/test/java/edu/zut/cs/score/finalscore/dao/FinalTestDaoTest.java
+++ score-dao/src/test/java/edu/zut/cs/score/finalscore/dao/FinalTestDaoTest.java
@@ -1,5 +1,29 @@
 package edu.zut.cs.score.finalscore.dao;
 
-public class FinalTestDaoTest {
+import edu.zut.cs.score.finalscore.domain.FinalTest;
+import edu.zut.cs.score.base.dao.GenericDaoTestCase;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+public class FinalTestDaoTest extends GenericDaoTestCase<Long, FinalTest, FinalTestDao>{
+
+	
+	private static final Logger logger = LogManager.getLogger(FinalTestDaoTest.class.getName());
+
+	@Autowired
+	FinalTestDao finaltestDao;
+
+	@Test
+	public void testFindAll() {
+
+		List<FinalTest> result = this.finaltestDao.findAll();
+		if (logger.isInfoEnabled()) {
+			logger.info("testFindAll() - List<User> result={}", result); //$NON-NLS-1$
+		}
+
+	}
 
 }
